3531f6fa35dda585c6d4b30c85eaedc717ccce59
George <george.groshev1997@gmail.com>
2023-03-25 11:59:09 +0200

    0.1

---
+++ src/main/java/com/george/spring/userManagmantSystem/UserManagmantSystemApplication.java
@@ -0,0 +1,12 @@
+package com.george.spring.userManagmantSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagmantSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagmantSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/george/spring/userManagmantSystem/UserManagmantSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.george.spring.userManagmantSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagmantSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
